c8d650dc659f15850c8b7556f59edaa2c0f5cfb5
Ali Siddiqi <alisiddiqui275@gmail.com>
2025-07-10 12:19:54 +0500

    experimenting with different base url

--- airbnb-scraper.py
+++ airbnb-scraper.py
@@ -65,7 +65,7 @@ def go_to_next_page():
 
 
 # base url
-url = "https://www.airbnb.com/s/Al-Aqeeq--Riyadh-Region--Saudi-Arabia/homes?flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2025-08-01&monthly_length=3&monthly_end_date=2025-11-01&refinement_paths%5B%5D=%2Fhomes&acp_id=7f314746-cda1-4ea3-b700-b8def06e2c61&date_picker_type=calendar&source=structured_search_input_header&search_type=autocomplete_click&price_filter_input_type=2&price_filter_num_nights=5&channel=EXPLORE&place_id=ChIJ_-JKUqjjLj4RkJgM30Rxoc0&location_bb=QcZZzkI6ltxBxgcuQjpzhw%3D%3D"
+url = "https://www.airbnb.com/s/Al-Narjis--Riyadh-Saudi-Arabia/homes?flexible_trip_lengths%5B%5D=one_week&monthly_start_date=2025-08-01&monthly_length=3&monthly_end_date=2025-11-01&date_picker_type=calendar&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&search_type=autocomplete_click&price_filter_input_type=2&price_filter_num_nights=5&channel=EXPLORE&place_id=ChIJb030nzX7Lj4RwIxfeQygX-c&location_bb=QcfU3UI6zfFBxnt8QjpaxA%3D%3D&acp_id=329e63d6-d6ef-4461-8078-f5a025ca60ce"
 driver.get(url)
 
 num_pages = 3 #int(input("How many pages do you want to scrape? "))
@@ -201,7 +201,7 @@ def scrape_details_page(url):
 
 
 # Function to save data to CSV using pandas
-def save_to_csv(data, filename='airbnb_riyadh_latest_test(2)_data.csv'):
+def save_to_csv(data, filename='airbnb_riyadh_najris_data.csv'):
     df = pd.DataFrame(data)
     df.to_csv(filename, index=False)
     print(f"Data saved to {filename}")
